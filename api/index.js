e57ea74a9ca96644e0856ff8750c74c549897419
Yash Meena <yashmeena1010@gmail.com>
2024-07-03 15:17:46 +0530

    connect to mongoDb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
-const app=express();
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
 
-app.get('/server',(req,res)=>{
-      res.send("Hello this is an backend server");
+const app=express();
+// to cofig the env file (we use env file variables file by using the dotenv package )
+//directly without dotenv package cant use the env file
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+.then(()=>{
+      console.log("MongoDb is connected");
 })
+.catch((err)=>{
+      console.log(err);
+})
+
 app.listen(3000,()=>{
       console.log("Server is running on the port 3000!!");
 })
