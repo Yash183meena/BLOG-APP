169345140fadb6671af59a4ceb2fe28515b0db57
Yash Meena <yashmeena1010@gmail.com>
2024-09-14 11:51:07 +0530

    complete user update functionality

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+
 
 export const test=(req,res)=>{
       res.json({message:"API is working!"});
@@ -19,19 +21,27 @@ export const updateUser=async(req,res,next)=>{
             req.body.password=bcryptjs.hashSync(req.body.password,10);
       }
 
-      if(req.body.username){
-            if(req.body.username.length<7 || req.body.username>20){
-                  return next(errorHandler(400,'Username must be between 7 and 20 characters'));
+      if (req.body.username) {
+            if (req.body.username.length < 7 || req.body.username.length > 20) {
+              return next(
+                errorHandler(400, 'Username must be between 7 and 20 characters')
+              );
             }
-      }
+            if (req.body.username.includes(' ')) {
+              return next(errorHandler(400, 'Username cannot contain spaces'));
+            }
+            if (req.body.username !== req.body.username.toLowerCase()) {
+              return next(errorHandler(400, 'Username must be lowercase'));
+            }
+            if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+              return next(
+                errorHandler(400, 'Username can only contain letters and numbers')
+              );
+            }
+          }
+    
 
-      if(req.body.username.includes(' ')){
-            return next(errorHandler(400,'Username cannot contain spaces'));
-      }
 
-      if(req.body.username!==req.body.username.toLowerCase()){
-            return next(errorHandler(400,'Username must be in the lower case'));
-      }
 
       try{
             const updatedUser=await User.findByIdAndUpdate(req.params.userId,{
@@ -50,3 +60,5 @@ export const updateUser=async(req,res,next)=>{
             next(error);
       }
 }
+
+    
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,10 +1,12 @@
 import { Alert, Button, TextInput } from 'flowbite-react';
 import { useEffect, useRef, useState } from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector,useDispatch} from 'react-redux';
 import {getDownloadURL, getStorage, uploadBytesResumable,ref} from 'firebase/storage';
 import { app } from '../firebase.js';
 import {CircularProgressbar} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { updateStart,updateSuccess,updateFailure } from '../redux/user/userSlice.js';
+
 
 const DashProfile = () => {  
    const {currentUser}=useSelector(state=>state.user);
@@ -13,6 +15,13 @@ const DashProfile = () => {
    const [imageFileUrl,setImageFileUrl]=useState(null);
    const [imageFileUploadProgress,setImageFileUploadProgress]=useState(null);
    const [imageFileUploadError,setImageFileUploadError]=useState(null);
+   const [formData,setFormData]=useState({});
+   const [imageFileUploading,setImageFileUploading]=useState(false);
+   const [updateUserSuccess,setUpdateUserSuccess]=useState(null);
+   const [updateUserError,setUpdateUserError]=useState(null);
+
+   const dispatch=useDispatch();
+
    const filePickerRef=useRef();
 
    console.log(imageFileUploadProgress,imageFileUploadError);
@@ -38,6 +47,7 @@ const DashProfile = () => {
     //   }
     //}
       console.log('upoading image...');
+      setImageFileUploading(true);
       setImageFileUploadError(null);
       const storage=getStorage(app);
       const fileName=new Date().getTime()+imageFile.name;
@@ -58,9 +68,15 @@ const DashProfile = () => {
         ()=>{
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
              setImageFileUrl(downloadURL);
+             setFormData({...formData,profilePicture:downloadURL});
+             setImageFileUploading(false);
           })
-        }
+        }  
       )
+   };
+
+   const handleChange=(e)=>{
+      setFormData({...formData,[e.target.id]:e.target.value});
    }
 
    useEffect(()=>{
@@ -69,12 +85,58 @@ const DashProfile = () => {
     }
    },[imageFile]);
 
+
+   const handleSubmit=async(e)=>{
+      e.preventDefault();
+      setUpdateUserError(null);
+      setUpdateUserSuccess(null);
+
+      if(Object.keys(formData).length==0){
+        setUpdateUserError('No changes made');
+        return;
+      }
+
+      if(imageFileUploading){
+        setUpdateUserError('Please wait for image to upload');
+        return;
+      }
+
+      try{
+          dispatch(updateStart());
+          const res=await fetch(`/api/user/update/${currentUser._id}`,{
+             method:'PUT',
+             headers:{
+                'Content-Type':'application/json',
+             },
+             body:JSON.stringify(formData),
+
+          });
+
+          const data=await res.json();
+          if(!res.ok){
+            dispatch(updateFailure(data.message));
+            setUpdateUserError(data.message);
+          }
+          else{
+            dispatch(updateSuccess(data));
+            setUpdateUserSuccess("User's profile updated successfully");
+          }
+      }
+      catch(error){
+           dispatch(updateFailure(error.message));
+           setUpdateUserError(error.message);
+      }
+   }
+
    //console.log(imageFile,imageFileUrl);
+   
+
+    
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
        <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-       <form className='flex flex-col gap-4'>
+       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
          <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
             <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>filePickerRef.current.click()}>
 
@@ -107,18 +169,28 @@ const DashProfile = () => {
             </div>
 
            {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
-            <TextInput type='username' id='username' placeholder='username' defaultValue={currentUser.username}/>
-            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
-            <TextInput type='password' id='password' placeholder='password'/>
+            <TextInput type='username' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
+            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange} />
+            <TextInput type='password' id='password' placeholder='password'onChange={handleChange}/>
              {/* outline matlab ki sirf hover karne par hi button me pura color fill hoga wanrna hover se phele tou sirf border pr hi colour dekhega */}
             <Button type='submit' gradientDuoTone="purpleToBlue" outline>
               Update
             </Button>
+            </form>
             <div className="text-red-500 flex justify-between mt-5">
               <span className="cursor-pointer">Delete Account</span>
               <span className="cursor-pointer">Sign Out</span>
             </div>
-       </form>
+            {updateUserSuccess && (
+              <Alert color='success' className='mt-5'>
+                 {updateUserSuccess}
+              </Alert>
+            )}
+            {updateUserError && (
+              <Alert color='failure' className='mt-5'>
+                  {updateUserError}
+              </Alert>
+            )}
     </div>
   )
 }
